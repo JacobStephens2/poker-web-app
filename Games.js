f39d06850be6e3ad690d9b0b272dd3d044a52649
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-21 16:32:04 -0400

    select open texas holdem button by onclick function

--- Games.js
+++ Games.js
@@ -2,5 +2,5 @@ function openTexasHoldEm() {
   gameName = "Texas Hold'Em"
   document.querySelector('h1').innerText = gameName;
   document.querySelector('title').innerText = gameName;
-  document.querySelector('#playTexasHoldEm').remove();
+  document.querySelector('button[onclick="openTexasHoldEm()"]').remove();
 }
