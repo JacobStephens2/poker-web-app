4e0fae11f90535308b7734dddb1cdb747477d86b
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-21 16:48:10 -0400

    add texas hold em script to page on openTexasHoldEm()

--- Games.js
+++ Games.js
@@ -1,6 +1,13 @@
 function openTexasHoldEm() {
+  // Retitle page with game name
   gameName = "Texas Hold'Em"
   document.querySelector('h1').innerText = gameName;
   document.querySelector('title').innerText = gameName;
+  // Remove game button
   document.querySelector('button[onclick="openTexasHoldEm()"]').remove();
+  // Link game script
+  const TexasHoldEmScript = document.createElement('script');
+  TexasHoldEmScript.src = "/games/TexasHoldEm.js";
+  TexasHoldEmScript.type = "module";
+  document.querySelector('head').append(TexasHoldEmScript);
 }
