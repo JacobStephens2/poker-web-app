45997681e370cf522cc3da2c8f2b98d3588e0565
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-21 17:06:05 -0400

    add card count to French Deck

--- components/FrenchDeck.js
+++ components/FrenchDeck.js
@@ -1,23 +1,34 @@
-// This French Deck creation code is from Walter Guevara
+// This French Deck creation code (createDeck() and the suits and values variables) 
+// is from Walter Guevara
 // at https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
 
 const suits = ["spades", "diamonds", "clubs", "hearts"];
 
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-function createFrenchDeck() {
+function createDeck() {
   let deck = new Array();
-
   for (let i = 0; i < suits.length; i++) {
     for (let x = 0; x < values.length; x++) {
       let card = { value: values[x], suit: suits[i] };
       deck.push(card);
     }
   }
-
   return deck;
 }
 
-const FrenchDeck = createFrenchDeck();
+function countCardsInDeck(deck) {
+  let cardCount = deck.length;
+  return cardCount;
+}
+
+let deck = createDeck();
+let cardCount = countCardsInDeck(deck);
+
+const FrenchDeck = {
+  cards: deck,
+  cardCount: cardCount
+  // shuffle: 
+}
 
 export default FrenchDeck;
